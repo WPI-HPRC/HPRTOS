595338d01653f7adbf4d42476d8989c52dc9c200
Dan Pearson <djpearson@wpi.edu>
2025-01-07 18:03:57 -0500

    Cleaned repository for update

--- apps/FlightComputer/FlightLib/FlightConfig.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr static int stateMachine_loopRate = 100; // [Hz]
-constexpr static int imu_looprate = 100; // [Hz]
-constexpr static int mag_looprate = 50; // [Hz]
-constexpr static int gps_looprate = 10; // [Hz]
-constexpr static int baro_looprate = 25; // [Hz]
